4a2b3ca277cff24c4ca183a64c61ab2445ae8148
harish kumar <harishpathania150@gmail.com>
2024-01-06 10:53:18 +0530

    news-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
-
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import News from '../pages/News';
-import NewsDetail from '../pages/NewsDetail';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import News from "../pages/News";
+import NewsDetail from "../pages/NewsDetail";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          'https://newsapi.org/v2/everything?q=tesla&from=2023-12-05&sortBy=publishedAt&apiKey=efccb86fb1864231adee426846191b80'
+          'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=efccb86fb1864231adee426846191b80'
         );
 
         if (!response.ok) {
-          throw new Error('Failed to fetch news');
+          throw new Error("Failed to fetch news");
         }
 
         const data = await response.json();
         setNews(data.articles);
       } catch (error) {
-        console.error('Error fetching news:', error);
+        console.error("Error fetching news:", error.message);
+        setError("Failed to fetch news. Please try again later.");
       }
     };
 
@@ -33,12 +34,20 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Routes>
-          
-            <Route path="/" element={  <News news={news} />}>
-            </Route>
-            <Route path="/news/:index" element={ <NewsDetail articles={news} />}>
-             
-            </Route>
+            <Route
+              path="/"
+              element={
+                error ? (
+                  <div className="error-message">{error}</div>
+                ) : (
+                  <News news={news} />
+                )
+              }
+            ></Route>
+            <Route
+              path="/news/:index"
+              element={<NewsDetail articles={news} />}
+            ></Route>
           </Routes>
         </header>
       </div>
